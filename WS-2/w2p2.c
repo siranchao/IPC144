910c3a2d78429937a423e53a95607ed628ca9589
Siran.CSR <79330375+siranchao@users.noreply.github.com>
2021-08-12 13:03:23 -0400

    Update w2p2.c

--- WS-2/w2p2.c
+++ WS-2/w2p2.c
@@ -1,12 +1,4 @@
-/*
-	==================================================
-	Workshop #2 (Part-2):
-	==================================================
-	Name   : Siran Cao
-	ID     : 159235209
-	Email  : scao34@myseneca.ca
-	Section: NAA
-*/
+
 #define _CRT_SECURE_NO_WARNINGS
 
 #include<stdio.h>
